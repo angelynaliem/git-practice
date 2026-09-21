575be2b8bbfc17f0f38944a789022c93a747b9e2
rayne <pothong.r@gmail.com>
2026-09-21 10:32:28 -0700

    implement function d

--- function_d.py
+++ function_d.py
@@ -2,7 +2,14 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    # branch testing hehe 
+    max_number = numbers[0]
+
+    for number in numbers:
+        if number > max_number:
+            max_number = number
+
+    return max_number
 
 
 if __name__ == "__main__":
