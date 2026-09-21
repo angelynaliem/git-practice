192c8bda907186b08840d60374a9eb92ca638fb8
Rahma Mahdi <rahmamahdi.25@gmail.com>
2026-09-21 10:25:15 -0700

    feat: function merged_list implemented

--- function_c.py
+++ function_c.py
@@ -3,8 +3,17 @@ def merge_lists(list_a, list_b):
         a combination of list_a and list_b
         without any duplicate elements.
     """
-    pass
+    merged = []
 
+    for item in list_a:
+        if item not in merged:
+            merged.append(item)
+
+    for item in list_b:
+        if item not in merged:
+            merged.append(item)
+
+    return merged
 
 if __name__ == "__main__":
     print(merge_lists([1, 1, 2, 3], [3, 4, 5]))
